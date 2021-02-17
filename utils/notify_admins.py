c53598c34c707f7702c8ccc4296b11f499ae9659
vladislavgrisyuk <vladislavgrisyuk@gmail.com>
2021-02-17 18:37:20 +0200

    changed config, removed admin notifications

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -7,8 +7,4 @@ from data.config import ADMINS
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Бот Запущен")
-
-        except Exception as err:
-            logging.exception(err)
+        a = 1 #Make this normal lol
